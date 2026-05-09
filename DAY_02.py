63640a620fc458af37f9e8c3f627108c9952ce73
Vedant021004 <vedantkp79@gmail.com>
2026-05-09 20:58:45 +0530

    heyy

---
+++ DAY_02.py
@@ -0,0 +1,22 @@
+# try:
+#     result = 10/0
+# except ZeroDivisionError:
+#     print("Cannot divide by zero!")
+# else:
+#     print("Result:", result)
+
+# try:
+#     x = 10 / 0
+
+# except Exception as e:
+#     print(e)
+
+
+
+# try:
+#     num = int("abc")
+# except ValueError:
+#     print("Invalid input! Please enter a valid integer.")
+# except Exception as e:
+#     print(e)
+
