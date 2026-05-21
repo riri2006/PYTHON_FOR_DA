450672c2a5df6659071a9a857d91b34e253655da
Vedant021004 <vedantkp79@gmail.com>
2026-05-21 16:31:39 +0530

    class

---
+++ class/class1.py
@@ -0,0 +1,16 @@
+# A class is used to group variables and functions together into one structure.
+
+class Dog:
+    is_animal = True
+    def bark():
+        print("woof!")
+
+dog = Dog()
+print(dog.is_animal)
+Dog.bark()
+
+
+
+
+
+    
