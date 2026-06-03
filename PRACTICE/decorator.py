e4bb66b005532209a2e31109f3c6d1c20efaa427
Vedant021004 <vedantkp79@gmail.com>
2026-06-03 21:13:57 +0530

    practice

--- PRACTICE/decorator.py
+++ PRACTICE/decorator.py
@@ -46,3 +46,34 @@ def add():
 
 
 print("Answer is :", add())
+
+# Before / After decorator.
+def devprator(func):
+    def wrapper():
+        return f"this is after"
+    func()
+
+    return wrapper
+
+
+@devprator
+def before():
+    print("this is before")
+    return f"hello"
+result = before()
+print(result)
+
+
+# Decorator for calculator functions.
+def before(func):
+    def after(x, y):  # ✅ accept arguments
+        print("Printing before")
+        return func(x, y)  # ✅ call original function here
+    return after
+
+@before
+def add(x, y):
+    return x + y
+
+adx = add(2, 3)
+print(adx)
--- PRACTICE/tempCodeRunnerFile.py
+++ PRACTICE/tempCodeRunnerFile.py
@@ -1,6 +1,12 @@
-def locker(api):
-    def key():
-        return api
-    return key
-check = locker(123123)
-print(check(1234))
+def before(func):
+    def after(x, y):  # ✅ accept arguments
+        print("Printing before")
+        return func(x, y)  # ✅ call original function here
+    return after
+
+@before
+def add(x, y):
+    return x + y
+
+adx = add(2, 3)
+print(adx)
